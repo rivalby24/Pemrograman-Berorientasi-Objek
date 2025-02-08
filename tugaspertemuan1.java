ec06eef0f842442735f12c8527ed5a5129581991
rivalby24 <muhammadrivalby@gmail.com>
2025-02-08 17:11:24 +0700

    Add files via upload

---
+++ tugaspertemuan1.java
@@ -0,0 +1,5 @@
+public class Pertemuan1{
+    public static void main(String args[]){
+        System.out.println("Hello World!, Saya Muhammad Rivalby dengan NIM 2307068. Saya Senang Belajar Java.");
+    }
+}
