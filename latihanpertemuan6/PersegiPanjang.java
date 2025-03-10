4d4924621527e9070fd0f474a63323c8039d8af3
rivalby24 <muhammadrivalby@gmail.com>
2025-03-10 20:56:25 +0700

    Tugas Pertemuan 6

---
+++ latihanpertemuan6/PersegiPanjang.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo.latihanpertemuan6;
+
+/**
+ *
+ * @author BlazeBy
+ */
+public class PersegiPanjang {
+    int panjang;
+    int lebar;
+    
+    PersegiPanjang(){
+        panjang = 0;
+        lebar = 0;
+    }
+    
+    PersegiPanjang(int panjangBaru, int lebarBaru){
+        panjang = panjangBaru;
+        lebar = lebarBaru;
+    }
+    
+    int getLuas(){
+        return panjang * lebar;
+    }
+    
+    int getKeliling(){
+        return (2 * panjang) + (2 * lebar);
+    }
+    
+    void setPanjangBaru(int panjangBaru){
+        panjang = panjangBaru;
+    }
+    
+    void setLebarBaru(int lebarBaru){
+        lebar = lebarBaru;
+    }
+    
+    
+}
